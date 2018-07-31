6d6f1528471ba296a46484d197374135040d46eb
Derek <dereklaumann@hotmail.com>
2018-07-31 19:23:50 -0400

    Joining html's

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, json, request
+import os
+
+app = Flask(__name__)
+
+#@app.route("/")
+#def hello():
+#	return render_template("index1.html")
+
+@app.route("/")
+def web():
+	return render_template('indexR.html')
+if __name__ == "__main__":
+	app.run(port = 5000)
+
+
